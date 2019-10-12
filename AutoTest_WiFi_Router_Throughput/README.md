ec382da00be8c0b2a27db99f706169afe61a2f2a
unknown <amyfanpti@gmail.com>
2019-10-12 23:44:04 +0800

    Add AutoTest_WiFi_Router_Throughput/test/Test_Chariot_CML.tcl

--- AutoTest_WiFi_Router_Throughput/README.md
+++ AutoTest_WiFi_Router_Throughput/README.md
@@ -3,12 +3,97 @@ Auto Test of WiFi Router Throughput Test by Selenium(via python) and Chariot(via
 
 # Table of Contents  
 
+[Reference](#reference)  
+[cmdline argument parsing using tcl?](#cmdline-argument-parsing-using-tcl)  
+
 # Troubleshooting
 
 
 # Reference
+## cmdline argument parsing using tcl?  
+[cmdline argument parsing using tcl? Jul 5, 2017](https://stackoverflow.com/questions/44917037/cmdline-argument-parsing-using-tcl)  
+
+```
+package require cmdline
+
+set parameters {
+    {s.arg ""   "Slot"}
+    {p.arg ""   "Port"}
+    {l.arg "100"   "Load"}
+    {f.arg "256"   "Framesize"}
+    {debug      "Turn on debugging, default=off"}
+}
+#set option(l) 100
+set usage "- A simple script to demo cmdline parsing"
+
+if {[catch {array set options [cmdline::getoptions ::argv $parameters $usage]}]} {
+    puts [cmdline::usage $parameters $usage]
+} else {
+    parray options
+}
+#puts [array get options]
+puts $options(l)
+puts $options(f)
+```
+```
+script Output:
+
+C:\Tcl\bin>tclsh opt.tcl -s 1 -f 128
+options(debug) = 0
+options(f)     = 128
+options(l)     = 100
+options(p)     =
+options(s)     = 1
+100
+128
+```
+[package for parsing argument in TCL Jun 21, 2014](https://stackoverflow.com/questions/24341141/package-for-parsing-argument-in-tcl)  
+```
+package require cmdline
+
+set parameters {
+    {server.arg ""   "Which server to search"}
+    {debug           "Turn on debugging, default=off"}
+}
+
+set usage "- A simple script to demo cmdline parsing"
+array set options [cmdline::getoptions ::argv $parameters $usage]
+parray options
+```
+```
+$ tclsh simple.tcl 
+options(debug)  = 0
+options(server) = 
+
+$ tclsh simple.tcl -server google.com
+options(debug)  = 0
+options(server) = google.com
+
+$ tclsh simple.tcl -server google.com -debug
+options(debug)  = 1
+options(server) = google.com
+
+$ tclsh simple.tcl -help
+simple - A simple script to demo cmdline parsing
+ -server value        Which server to search <>
+ -debug               Turn on debugging, default=off
+ -help                Print this message
+ -?                   Print this message
+
+    while executing
+"error [usage $optlist $usage]"
+    (procedure "cmdline::getoptions" line 15)
+    invoked from within
+"cmdline::getoptions ::argv $parameters $usage"
+    invoked from within
+"array set options [cmdline::getoptions ::argv $parameters $usage]"
+    (file "simple.tcl" line 11)
+```
 
 
+```
+
+```
 * []()
 ![alt tag]()
 
---
+++ AutoTest_WiFi_Router_Throughput/test/Test_Chariot_CML.tcl
@@ -0,0 +1,83 @@
+################################################################################
+# May012019 Initailization.
+# May122019 for Chariot thruput test.
+# May182019 Adapt from Test_GUI_02.tcl
+# Oct122019 Adapt from Test_Chariot_GUI_02.tcl
+################################################################################
+package require Tcl 8.5
+
+set currpath [file dirname [file normalize [info script]]];#[file dirname [info script]]
+set lib_path [regsub -all "test" $currpath "lib"];#[file join  ".." "lib"]
+set log_path  [regsub -all "test" $currpath "logs"]
+lappend auto_path $lib_path
+source "$lib_path/API_Misc.tcl"
+source "$lib_path/API_GUI.tcl"
+package require cmdline
+
+################################################################################
+# CML parsing Procedure
+################################################################################
+set parameters {
+    {config_ini.arg ""   "Assing configuration INI file."}
+    {direction.arg ""   "LAN<-->WAN or LAN-->WAN or LAN<--WAN."}
+    {moduration.arg ""   "11n or 11ac."}
+    {debug           "Turn on debugging, default=off"}
+}
+
+set usage "- A Tcl script to  excute Chairot by cmdline"
+array set options [cmdline::getoptions ::argv $parameters $usage]
+parray options
+
+set Func_INI::currPath $currpath
+set Func_INI::log_Path $log_path
+set Func_INI::lib_Path $lib_path
+set inifile [file join $Func_INI::lib_Path "chariot_config_ini" $options(config_ini)]
+#puts $inifile
+################################################################################
+# Get INI file paramter
+################################################################################
+set Func_INI::verbose on;#on off
+set Func_INI::logfile [file join $log_path inifile.log]
+
+Func_INI::_GetChariot_Param $inifile
+
+
+# source Chariot related API
+set path_chariot [dict get $Func_INI::dict_Chariot_Param "chariot_path"];#get chariot directory
+lappend auto_path $path_chariot
+source "$lib_path/API_Chariot.tcl"
+
+set Func_Chariot::verbose on;#on off
+set Func_Chariot::logfile [file join $log_path chariot.log]
+
+
+################################################################################
+# https://www.doulos.com/knowhow/tcltk/examples/buttons/
+################################################################################
+
+################################################################################
+# Start Here
+################################################################################
+set COPYRIGHT {
+    Copyright (c) 2019, 2020 QA,
+    This program is free to modify, extend at will.  The author(s)
+    provides no warrantees, guarantees or any responsibility for usage.
+}
+set VERSION "    If you have question, please contact QA Dept."
+
+array set pref [list \
+        debug		0 \
+        usesession	1 \
+        sessionfile	[file join [file normalize $env(HOME)] .tksqlite]\
+        appname		"Chariot Thruput Test"  \
+        enable_encoding	[lsort -dictionary [encoding names]] \
+        recent_file [list] \
+        openTypeSqlite	[list {"SQLite Files" {.db .db2 .db3 .sqlite}} {"All Files" *}] \
+        openTypeSql		[list {"SQL Files" {.sql}} {"All Files" *}] \
+        openTypeText	[list {"Text Files" {.txt .csv .tab .tsv .dat}} {"All Files" *}] \
+        ]
+
+
+################################################################################
+# Run Here
+################################################################################
