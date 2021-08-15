7cbcc645c841d64c97712fb537f1616758fb5260
philip.shen <amyfanpti@gmail.com>
2021-08-15 11:45:17 +0800

    [update] update Acoustic_Signal/test/test_db.py

--- Acoustic_Signal/test/test_db.py
+++ Acoustic_Signal/test/test_db.py
@@ -1,9 +1,18 @@
+import os, sys
+
+sys.path.append('../lib')
+
 import db_function as dbf
  
-x = 2
-dBref = 2e-5
-y_db = dbf.db(x, dBref)
-y_lin = dbf.idb(y_db, dBref)
- 
-print(y_db)
-print(y_lin)
+list_x= [2, 11.885]
+list_y_db= [6, 21.5]
+dBref = 1;#2e-5
+
+for y_db in list_y_db:
+    #y_db = dbf.db(x, dBref)
+    y_lin = dbf.idb(y_db, dBref)
+    """
+    6.0 dB; line:1.995
+    21.5 dB; line:11.885
+    """    
+    print("{:.1f} dB; line:{:.3f} ".format(y_db, y_lin) )
