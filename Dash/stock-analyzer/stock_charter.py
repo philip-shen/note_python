5ce0f99e0f127b62a951078efa3b281a732b8652
unknown <amyfanpti@gmail.com>
2022-09-29 21:20:15 +0800

    [update] Dash/dash-technical-charting

--- Dash/stock-analyzer/stock_charter.py
+++ Dash/stock-analyzer/stock_charter.py
@@ -376,7 +376,7 @@ class Engine:
         def update_table(n_clicks, ticker):
             """Updates the metrics found on asset info"""
 
-            if ticker not in TICKER_LIST:
+            if ticker not in    :
                 raise Exception("Invalid Ticker!")
             else:
                 pass
