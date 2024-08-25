18fbab373cf0529844c07ef00360b65bde05725b
fanpti amy <amyfanpti@gmail.com>
2024-08-26 00:57:31 +0800

    [update] Chart_Trend/StockInsider/test_stock_indicator.py

--- Chart_Trend/StockInsider/test_stock_indicator.py
+++ Chart_Trend/StockInsider/test_stock_indicator.py
@@ -23,6 +23,12 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import numpy as np
 import matplotlib.pyplot as plt
 from arch import arch_model
+import argparse
+import pathlib
+import requests
+from requests.exceptions import Timeout
+from io import StringIO
+import json
 
 import twseotc_stocks.lib_misc as lib_misc
 from insider.logger_setup import *
@@ -37,6 +43,35 @@ def est_timer(start_time):
     time_consumption, h, m, s= lib_misc.format_time(time.time() - start_time)         
     msg = 'Time Consumption: {}.'.format( time_consumption)#msg = 'Time duration: {:.2f} seconds.'
     logger.info(msg)
+
+# Change the date
+#############################################
+def date_changer( date):
+    
+    year = date[:4]
+    year = str(int(year)-1911)
+    month = date[4:6]
+    day = date[6:]
+        
+    return year+"/"+month+"/"+day
+
+### waste 3~4 sec to request so move main routine
+def requests_twse_tpex_stock_idx(json_data):
+    ##### 上市公司
+    datestr = json_data["lastest_datastr_twse_tpex"]#'20240801'
+    r = requests.post('https://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date=' + datestr + '&type=ALL')
+    # 整理資料，變成表格
+    df_twse_website_info = pd.read_csv(StringIO(r.text.replace("=", "")), header=["證券代號" in l for l in r.text.split("\n")].index(True)-1)
+        
+    ##### 上櫃公司
+    datestr = date_changer(json_data["lastest_datastr_twse_tpex"])#'113/08/01'
+    r = requests.post('http://www.tpex.org.tw/web/stock/aftertrading/daily_close_quotes/stk_quote_download.php?l=zh-tw&d=' + datestr + '&s=0,asc,0')
+    # 整理資料，變成表格
+    df_tpex_website_info = pd.read_csv(StringIO(r.text), header=2).dropna(how='all', axis=1).dropna(how='any')
+        
+    logger.info("Request TWSE and TPEX Stock index..")
+    
+    return [df_twse_website_info, df_tpex_website_info]
 
 # データフレームを初期化
 columns = ['Ticker', 'PBR', 'Revenue Growth', 'Profit Growth', 'ROE', 'PER', 'Volume', 'Operating Cash Flow']
@@ -178,7 +213,26 @@ def calculate_volume_weighted_average_price(data):
     data['cum_volume_price'] = (data['Close'] * data['Volume']).cumsum()
     data = data['cum_volume_price'] / data['cum_volume']
     return data
+
+def stand_Up_On_MAs(data):
+    logger.info("{}".format("Stand_Up_On_MAs (針對你Fetch data區間的最後一天做分析):"))
+
+    # 抓出所需data
+    stock_price = data['Close'].astype(float).iloc[-1]
+    MA5 = data['MA_5'].iloc[-1] if not data['MA_5'].isnull().values.all() else 0
+    MA10 = data['MA_10'].iloc[-1] if not data['MA_10'].isnull().values.all() else 0
+    MA20 = data['MA_20'].iloc[-1] if not data['MA_20'].isnull().values.all() else 0
+    MA60 = data['MA_60'].iloc[-1] if not data['MA_60'].isnull().values.all() else 0
+
+    four_MAs = MA5 and MA10 and MA20 and MA60
+    three_MAs = MA5 and MA10 and MA20
         
+    four_flag = True if four_MAs and max(stock_price, MA5, MA10, MA20, MA60) == stock_price else False
+    three_flag = True if three_MAs and max(stock_price, MA5, MA10, MA20) == stock_price else False
+
+    return four_flag, three_flag, four_MAs, three_MAs
+    
+                    
 def stock_price_graph(tickers: list, start_date: str, end_date: str):
     # グラフのサイズを小さくして複数表示
     plt.figure(figsize=(tickers.__len__(), 60))
@@ -230,7 +284,7 @@ def stock_price_graph(tickers: list, start_date: str, end_date: str):
         ax1.plot(data.index, data['MA_5'], label='5-Day MA', linestyle='-', color='blue')
         ax1.plot(data.index, data['MA_20'], label='20-Day MA', linestyle='-', color='red')
         #ax1.plot(data.index, data['MA_10'], label='10-Day MA', linestyle='-', color='yellow')
-        ax1.plot(data.index, data['MA_60'], label='60-Day MA', linestyle='-', color='brown')
+        ax1.plot(data.index, data['MA_60'], label='60-Day MA', linestyle='-', color='brown') 
         ax1.set_title(f'{ticker} - Close Price, VWAP, Moving Averages')
         ax1.set_xlabel('Date')
         ax1.set_ylabel('Price')
@@ -253,10 +307,26 @@ def stock_price_graph(tickers: list, start_date: str, end_date: str):
         ax2.tick_params(axis='x', rotation=45)
 
         #data.to_csv(f'{dirnamelog}/{ticker}_indicators.csv', index=False)
-
+        stand_Up_On_MAs(data)
+        
     plt.show()
+
+def date_changer_twse( date):
+    
+    year = date[:4]
+    year = str(int(year)-1911)
+    month = date[4:6]
+    day = date[6:]
+        
+    return year+"-"+month+"-"+day
     
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='plot stock chart trend')
+    parser.add_argument('--conf_json', type=str, default='config.json', help='Config json')
+    parser.add_argument('--gspred_json', type=str, default='xxxx.json', help='Google Sheet Certi json')
+    
+    args = parser.parse_args()
+    
     logger_set(strdirname)
     
     # Get present time
@@ -266,7 +336,66 @@ if __name__ == '__main__':
     logger.info(msg.format( local_time.tm_year,local_time.tm_mon,local_time.tm_mday,\
                             local_time.tm_hour,local_time.tm_min,local_time.tm_sec))
     
+    json_file= args.conf_json
+    
+    json_path_file = pathlib.Path(strdirname)/json_file
+    
+    if (not os.path.isfile(json_file))  :
+        msg = 'Please check json file:{}  if exist!!! '
+        logger.info(msg.format(json_file) )    
+        est_timer(t0)
+        sys.exit()
+
+    with open(json_file, encoding="utf-8") as f:
+        json_data = json.load(f)  
+        
     opt_verbose= 'OFF'
+    
+    list_df_twse_tpex_stock_idx = requests_twse_tpex_stock_idx(json_data)
+    
+    df_twse_stock_idx = list_df_twse_tpex_stock_idx[0]
+    df_tpex_stock_idx = list_df_twse_tpex_stock_idx[1]
+    
+    
+    df_twse_ticker = df_twse_stock_idx['證券代號'].copy()+'.TW'
+    df_tpex_ticker = df_tpex_stock_idx['代號'].copy()+'.TWO'
+    
+    if opt_verbose.lower() == 'on':
+        logger.info(f'df_twse_ticker:\n {df_twse_ticker}' )    
+        logger.info(f'df_tpex_ticker:\n {df_tpex_ticker}' )    
+    
+    start_date = date_changer_twse(json_data["start_end_date"][0])
+    end_date = date_changer_twse(json_data["start_end_date"][1])
+    for i, ticker in enumerate(df_twse_ticker.to_list()):
+        data = yf.download(ticker, start=start_date, end=end_date, interval="1d")
+        
+        logger.info(f"ticker: {ticker}; stock name: {df_twse_stock_idx['證券名稱'][i]}")        
+        
+        if data.empty:
+            continue
+
+        # 必要な列を抽出
+        data = data[['Close', 'Volume', 'High', 'Low']].copy()
+        
+        # 移動平均線を計算
+        data = calculate_moving_averages(data)
+        
+        #logger.info(f'data_moving_averages:\n {data}' )    
+        
+        four_flag, three_flag, four_MAs, three_MAs = stand_Up_On_MAs(data) 
+        
+        # 判斷data值
+        if four_flag:
+            logger.info("股價已站上5日、10日、20日、60日均線均線，為四海遊龍型股票!!")
+        elif three_flag:
+            logger.info("股價已站上5日、10日、20日均線，為三陽開泰型股票!!")
+        #elif not four_MAs:
+        #    logger.info("目前的data數量不足以畫出四條均線，請補足後再用此演算法!!")
+        #elif not three_MAs:
+        #    logger.info("目前的data數量不足以畫出三條均線，請補足後再用此演算法!!")
+        else:
+            logger.info("目前股價尚未成三陽開泰型、四海遊龍型股票!!")
+    '''
     # 銘柄リスト
     tickers_JP = [
     '1332.T',  # 日本水産
@@ -288,6 +417,7 @@ if __name__ == '__main__':
     ]
    
     tickers_TW = [
+        '4755.TW',
         '2330.TW',  
         '2303.TW',  
     ]
@@ -296,7 +426,7 @@ if __name__ == '__main__':
     # 結果を表示
     print(df)
     
-    stock_price_graph(tickers=tickers_TW, start_date="2024-01-01", end_date="2024-08-25")
-    
+    stock_price_graph(tickers=tickers_TW, start_date="2023-10-01", end_date="2024-08-25")
+    '''
     
     est_timer(t0)    
